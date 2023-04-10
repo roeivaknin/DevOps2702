4ee708eb71b26d3e30049ea4719058be131c1a15
roeiva <roei7800@gmail.com>
2023-04-10 18:23:56 +0300

    another commit

--- 10.py
+++ 10.py
@@ -1,5 +1,8 @@
 from fibo import fib as first_fib
 from roeifibo import fib as second_fib
-first_fib(100)
+first_fib(100) # test
 second_fib(100)
+
+
+
 
--- 5.py
+++ 5.py
@@ -1,10 +1,11 @@
 # A
-first = 7
+first = 8
 second = 44.3
 print(first + second)
 print(first * second)
 print(second / first)
 
+aaa = "bbb"
 # B
 # a = 9, b = 8, c = 15
 
@@ -12,7 +13,7 @@ print(second / first)
 # No. It might be a different if it was 1 charcater and is so it will represents a char.
 
 my_number = 5+5
-print(f"result is: {my_number}")
+print(f"result is: {my_number}") # test for git
 
 # D
 # x = 7
